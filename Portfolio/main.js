f97b8373031f6b17b57fed98f346742e5736de03
JaehoJeong <76611420+jaeho0613@users.noreply.github.com>
2021-01-23 20:07:09 +0900

    프로젝트 Flex
    - 스크롤시 Navbar Active 표시
    - Arrow Button 처리
    - Contact Me Button 처리

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,15 +1,8 @@
-// 개발자의 실수를 잡아줌
-// - 흔히 발생하는 코딩 실수를 잡아내서 예외를 발생시킵니다.
-// - 상대적으로 안전하지 않은 액션이 발생하는 것을 방지하거나 그럴 때 예외를 발생시킵니다. 예를 들자면 전역객체들에 접근하려 한다거나 하는 것들이겠지요.
-// - 혼란스럽거나 제대로 고려되지 않은 기능들을 비활성화시킵니다.
-"use stric";
+"use strict";
 
-// Navbar 이벤트
-const navbar = document.querySelector("#navbar"); // Navbar 가져오기
-const navbarHeight = navbar.getBoundingClientRect().height; // Navbar 높이 가져오기
-
-// 전체 이벤트
-// - 스크롤이 될 때 Navbar 높이를 판별하여 색 변경
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
@@ -18,7 +11,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
-//  Navbar 클릭 이벤트
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
@@ -28,28 +21,29 @@ navbarMenu.addEventListener("click", (event) => {
   }
   navbarMenu.classList.remove("open");
   scrollIntoView(link);
+  selectNavItem(target);
 });
 
-// Navbar 토글 버튼
+// Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector(".navbar__toggle-button");
 navbarToggleBtn.addEventListener("click", () => {
   navbarMenu.classList.toggle("open");
 });
 
-// Contact Me Button 클릭 이벤트
+// Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-// Home 화면 페이드 아웃
+// Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Arrow Up Show
+// Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
@@ -59,7 +53,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// Arrow Up Click
+// Handle click on the "arrow up" button
 arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
 });
@@ -68,22 +62,20 @@ arrowUp.addEventListener("click", () => {
 const workBtnContainer = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
-workBtnContainer.addEventListener("click", (event) => {
-  const filter =
-    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
     return;
   }
 
-  // Remove Selection
+  // Remove selection from the previous item and select the new one
   const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
-  const target =
-    event.target.nodeName === "BUTTON" ? event.target : event.target.parentNode;
-  target.classList.add("selected");
+  if (active != null) {
+    active.classList.remove("selected");
+  }
+  e.target.classList.add("selected");
 
   projectContainer.classList.add("anim-out");
-
   setTimeout(() => {
     projects.forEach((project) => {
       console.log(project.dataset.type);
@@ -93,13 +85,70 @@ workBtnContainer.addEventListener("click", (event) => {
         project.classList.add("invisible");
       }
     });
-
     projectContainer.classList.remove("anim-out");
   }, 300);
 });
 
-// 스크롤 이벤트 메서드
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonials",
+  "#contact",
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected;
+  selectedNavItem.classList.add("active");
+}
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener("wheel", () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
